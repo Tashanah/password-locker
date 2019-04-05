9826eec729d1d241835d9f9b1ae201b43ee055c6
Tashanah Muhando <tashanah.muhando@gmail.com>
2019-04-05 15:12:17 +0300

    credentials class created

--- users.py
+++ users.py
@@ -2,5 +2,8 @@ class user:
     def _init_(self,user_name,account_name):
         self.user_name=user_name
         self.account_name=account_name
-        
 
+
+class credentials:
+    def _init_(self,user_password):
+        self.user_password=user_password 
