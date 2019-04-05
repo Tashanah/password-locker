dba089119428d2e8e729bfc5ca42989d28ee0fbf
Tashanah Muhando <tashanah.muhando@gmail.com>
2019-04-05 15:34:53 +0300

    array created for user class and credentials class

--- users.py
+++ users.py
@@ -2,7 +2,7 @@ class user:
     '''
     class that generates new instance of a user
     '''
-    # user_list=[]
+    user_list=[]
 
     def _init_(self,user_name,account_name):
         self.user_name=user_name
@@ -16,7 +16,7 @@ class credentials:
     '''
     class that generates new password
     '''
-    # credentials_list=[]
+    credentials_list=[]
     def _init_(self,user_password):
         self.user_password=user_password 
 print("enter password.It must be a mix of 12 numbers and letters(uppercase and lowercase)")
